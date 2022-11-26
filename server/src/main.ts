3007a1c74c99cd77400428ce2ad76f52f607c87b
Ivan-Dobei <dobej.work@gmail.com>
2022-11-26 17:59:55 +0200

    add users module, roles module

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function start() {
-  const PORT: number | string = process.env.PORT || 5000;
+  const PORT = Number(process.env.PORT) || 5000;
   const app = await NestFactory.create(AppModule);
 
   await app.listen(PORT, () => {
---
+++ server/src/roles/roles.model.ts
@@ -0,0 +1,33 @@
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { UserRoles } from './user-roles.model';
+
+interface RolesCreationAttrs {
+  value: string;
+  description: string;
+}
+
+@Table({ modelName: 'roles' })
+export class Role extends Model<Role, RolesCreationAttrs> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  value: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  description: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
+}
